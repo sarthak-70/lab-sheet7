60338203450a0ec9706628bdfc8cf5ac7320e403
sarthak-70 <cu25720001@coeruniversity.ac.in>
2025-11-11 10:05:56 +0530

    Copy one string to another

---
+++ 3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+    char str1[100], str2[100];
+    cout << "Enter a string: ";
+    cin.getline(str1, 100);
+    strcpy(str2, str1);
+    cout << "Copied string: " << str2;
+    return 0;
+}
