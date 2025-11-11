db7143afb677bc7e60b05af1b7be75d8e64a2756
sarthak-70 <cu25720001@coeruniversity.ac.in>
2025-11-11 10:14:03 +0530

    Count number of words

---
+++ 16.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+    char str[200];
+    int words = 1;
+    cout << "Enter a sentence: ";
+    cin.getline(str, 200);
+    for (int i = 0; i < strlen(str); i++)
+        if (str[i] == ' ' && str[i+1] != ' ')
+            words++;
+    cout << "Number of words: " << words;
+    return 0;
+}
